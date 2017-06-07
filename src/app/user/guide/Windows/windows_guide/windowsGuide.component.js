bb8f824e4c382231cd9950f87d6d208ef04a4c03
Oleksandr <gulenkos@gmail.com>
2017-06-07 18:15:25 +0300

    Initial version of usb only Windows refresh

--- src/app/user/guide/Windows/windows_guide/windowsGuide.component.js
+++ src/app/user/guide/Windows/windows_guide/windowsGuide.component.js
@@ -1,3 +1,25 @@
-/**
- * Created by user on 07.06.2017.
- */
+(function() {
+    'use strict';
+
+    angular
+        .module('app.user')
+        .component('windowsGuide',
+            {
+                bindings: {
+                    item: '<'
+                },
+                controller: windowsGuideController,
+                controllerAs: 'vm',
+                templateUrl: 'app/user/guide/Windows/windows_guide/windowsGuide.template.html'
+            }
+        );
+
+    windowsGuideController.$inject = [];
+
+    function windowsGuideController() {
+        var vm = this;
+        vm.test = 'test';
+        console.log(vm.item);
+    }
+})();
+
