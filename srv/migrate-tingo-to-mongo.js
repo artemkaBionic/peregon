285cdc1b42909ef0f8e9547b188fa6a92c32e535
Larry Olsen <larry@basechord.com>
2018-02-12 11:49:44 -0700

    Improve migrate tingo to mongo

--- srv/migrate-tingo-to-mongo.js
+++ srv/migrate-tingo-to-mongo.js
@@ -7,7 +7,10 @@ module.exports = function(io) {
     var tingoDB = new tingoEngine.Db(config.kioskDataPath, {});
     var tingoSessions = tingoDB.collection('sessions');
     var mongoSession = require('./models/session.js')(io);
+    var path = require('path');
+    var shell = require('shelljs');
     var winston = require('winston');
+    var Promise = require('bluebird');
 
     function migrateSession(session) {
         var newSession = JSON.parse(JSON.stringify(session)); //Clone object so we don't modify the original
@@ -16,18 +19,21 @@ module.exports = function(io) {
         delete newSession.device.description;
         if (newSession.failedTests !== undefined) {
             newSession.failed_tests = newSession.failedTests;
+            delete newSession.failedTests;
         }
         for (var i = 0, len = newSession.logs.length; i < len; i++) {
             if (newSession.logs[i].details) {
                 newSession.logs[i].details = JSON.stringify(newSession.logs[i].details).replace(/^\s*{*\s*|\s*}*\s*$/g, '').replace('failedTests', 'Failed tests');
             }
         }
+        if (newSession.status === 'Incomplete') {
+            newSession.status = 'Success';
+        }
         return mongoSession.findOneAndUpdate({'start_time': newSession.start_time}, newSession, {upsert: true}).then(function() {
             return new Promise(function(resolve, reject) {
-                session.is_migrated = true;
-                tingoSessions.update({'_id': session._id}, session, {upsert: false}, function(err) {
+                tingoSessions.remove({'_id': session._id}, function(err) {
                     if (err) {
-                        winston.error('Can not set is_migrated for session ' + session._id + ' in TingoDB', err);
+                        winston.error('Failed to remove session ' + session._id + ' from TingoDB', err);
                         reject(err);
                     } else {
                         resolve();
@@ -39,30 +45,15 @@ module.exports = function(io) {
         })
     }
 
-    function fixIncompleteSessions() {
-        var sixHoursAgo = new Date();
-        sixHoursAgo.setHours(sixHoursAgo.getHours() - 6);
-        mongoSession.updateMany({'status': 'Incomplete', 'start_time': {$lt: sixHoursAgo}}, {$set: {'status': 'Success'}}).then(function(raw) {
-            winston.info('Fix incomplete sessions is finished', raw);
-        }).catch(function(err) {
-            winston.error('Failed to fix incomplete sessions', err);
-        });
-    }
-
     function migrate() {
-        tingoSessions.find({'is_migrated': {$exists: false}}).toArray(function(err, sessions) {
+        shell.cp('-n', path.join(config.kioskDataPath, 'sessions'), path.join(config.kioskDataPath, 'sessions.bak'));
+        tingoSessions.find({}).toArray(function(err, sessions) {
             if (err) {
                 winston.error('Failed to read sessions from TingoDB', err);
             } else {
                 if (sessions.length > 0) {
-                    var promises = [];
-                    winston.info('Attempting to import ' + sessions.length + ' sessions from TingoDB');
-                    for (var i = 0, len = sessions.length; i < len; i++) {
-                        promises.push(function(session) {
-                            migrateSession(session)
-                        }(sessions[i]));
-                    }
-                    return Promise.all(promises).then(fixIncompleteSessions);
+                    winston.warn('Importing ' + sessions.length + ' sessions from TingoDB');
+                    Promise.mapSeries(sessions, migrateSession);
                 } else {
                     winston.info('No sessions need to be migrated from TingoDB');
                     return Promise.resolve();
