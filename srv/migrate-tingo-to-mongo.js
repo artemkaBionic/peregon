28b027aad1aaf6de0d93405744f30516f9723c2f
Larry Olsen <larry@basechord.com>
2018-02-12 09:55:01 -0700

    Fix incomplete sessions while migrating from TingoDB to MongoDB

--- srv/migrate-tingo-to-mongo.js
+++ srv/migrate-tingo-to-mongo.js
@@ -22,16 +22,31 @@ module.exports = function(io) {
                 newSession.logs[i].details = JSON.stringify(newSession.logs[i].details).replace(/^\s*{*\s*|\s*}*\s*$/g, '').replace('failedTests', 'Failed tests');
             }
         }
-        mongoSession.findOneAndUpdate({'start_time': newSession.start_time}, newSession, {upsert: true}).then(function() {
-            session.is_migrated = true;
-            tingoSessions.update({'_id': session._id}, session, {upsert: false}, function(err) {
-                if (err) {
-                    winston.error('Can not set is_migrated for session ' + session._id + ' in TingoDB', err);
-                }
+        return mongoSession.findOneAndUpdate({'start_time': newSession.start_time}, newSession, {upsert: true}).then(function() {
+            return new Promise(function(resolve, reject) {
+                session.is_migrated = true;
+                tingoSessions.update({'_id': session._id}, session, {upsert: false}, function(err) {
+                    if (err) {
+                        winston.error('Can not set is_migrated for session ' + session._id + ' in TingoDB', err);
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
             });
         }).catch(function(err) {
             winston.error('Failed to migrate session ' + session._id + ' from TingoDB', err);
         })
+    }
+
+    function fixIncompleteSessions() {
+        var sixHoursAgo = new Date();
+        sixHoursAgo.setHours(sixHoursAgo.getHours() - 6);
+        mongoSession.updateMany({'status': 'Incomplete', 'start_time': {$lt: sixHoursAgo}}, {$set: {'status': 'Success'}}).then(function(raw) {
+            winston.info('Fix incomplete sessions is finished', raw);
+        }).catch(function(err) {
+            winston.error('Failed to fix incomplete sessions', err);
+        });
     }
 
     function migrate() {
@@ -39,9 +54,18 @@ module.exports = function(io) {
             if (err) {
                 winston.error('Failed to read sessions from TingoDB', err);
             } else {
-                winston.info('Attempting to import ' + sessions.length + ' sessions from TingoDB');
-                for (var i = 0, len = sessions.length; i < len; i++) {
-                    migrateSession(sessions[i]);
+                if (sessions.length > 0) {
+                    var promises = [];
+                    winston.info('Attempting to import ' + sessions.length + ' sessions from TingoDB');
+                    for (var i = 0, len = sessions.length; i < len; i++) {
+                        promises.push(function(session) {
+                            migrateSession(session)
+                        }(sessions[i]));
+                    }
+                    return Promise.all(promises).then(fixIncompleteSessions);
+                } else {
+                    winston.info('No sessions need to be migrated from TingoDB');
+                    return Promise.resolve();
                 }
             }
         });
